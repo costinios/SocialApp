cf1633a222664dbf1df124e5ee79c1a4f2c1c883
Costin Valu <mymac@Costins-MacBook-Pro.local>
2018-01-07 15:16:49 +0200

    created project with Firebase included

---
+++ SocialApp/Controller/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Costin Valu on 1/7/18.
+//  Copyright © 2018 Costin Valu. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
